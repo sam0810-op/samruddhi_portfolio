a09b1fb1c7d90530006c24a12aadb2cd0d4053ee
samruddhi khandekar <samukhandekar08@gmail.com>
2026-01-02 22:46:19 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+    reveals.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            section.classList.add("active");
+        }
+    });
+});
